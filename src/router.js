eb1487449642ae28dd6657fcdc51dec22602072c
ttrevi20 <ttrevi20@stu.southtexascollege.edu>
2026-03-08 15:53:49 -0500

    cleanup

--- src/router.js
+++ src/router.js
@@ -8,11 +8,6 @@ const routes = [
     name: 'home',
     component: HomePage,
   },
-  {
-    path: '/other',
-    name: 'other',
-    component: () => import('@/views/OtherPage.vue'),
-  },
 ]
 
 const router = createRouter({
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,8 @@
 <script setup>
-import { ref } from 'vue'
-const title = ref('Hello There ✋')
+
 </script>
 
 <template>
   <main class="flex flex-1 items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
   </main>
 </template>
